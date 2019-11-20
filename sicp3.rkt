ccf24ff4000cb227bafb1e8fc298bff57b6bd805
Jordan Grace <jordan@righteye.com>
2019-11-20 12:51:48 -0500

    Caught up to 3.2

--- sicp3.rkt
+++ sicp3.rkt
@@ -74,6 +74,14 @@
         (lambda (dummy) "Incorrect password")))
   dispatch)
 
+; 3.1.3 -- The Costs of Introducing Assignment
+
+; The trouble here is that substitution is based ultimately on the notion that the symbols in our language are essentially names for values. 
+; But as soon as we introduce set! and the idea that the value of a variable can change, a variable can no longer be simply a name. 
+; Now a variable somehow refers to a place where a value can be stored, and the value stored at this place can change. 
+; In 3.2 we will see how environments play this role of “place” in our computational model. 
+
+
 
 ; Queue
 
